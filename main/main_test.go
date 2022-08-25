a1955f9502c1ba9d4348fa9b1508faaf72df59c1
kotaro yamada <yamadakotaro@team-lab.com>
2022-08-26 00:46:09 +0900

    [fix]テストの際runメソッドにlistenerを引数に渡せるように

--- main/main_test.go
+++ main/main_test.go
@@ -5,23 +5,56 @@ import (
 	"fmt"
 	"golang.org/x/sync/errgroup"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 )
 
+//	func TestRun(t *testing.T) {
+//		ctx, cancel := context.WithCancel(context.Background())
+//		eg, ctx := errgroup.WithContext(ctx)
+//		eg.Go(func() error {
+//			return run(ctx)
+//		})
+//		in := "message"
+//		rsp, err := http.Get("http://localhost:18080/" + in)
+//		if err != nil {
+//			t.Errorf("failed to get: %+v", err)
+//		}
+//		defer rsp.Body.Close()
+//		got, err := io.ReadAll(rsp.Body)
+//		if err != nil {
+//			t.Fatalf("failed to read body: %v", err)
+//		}
+//		want := fmt.Sprintf("Hello, %s!", in)
+//		if string(got) != want {
+//			t.Errorf("want %q, but got %q", want, got)
+//		}
+//		cancel()
+//		if err := eg.Wait(); err != nil {
+//			t.Fatal(err)
+//		}
+//	}
 func TestRun(t *testing.T) {
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port %v", err)
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, listener)
 	})
 	in := "message"
-	rsp, err := http.Get("http://localhost:18080/" + in)
+	url := fmt.Sprintf("http://%s/%s", listener.Addr().String(), in)
+	t.Logf("try request to %q", url)
+	response, err := http.Get(url)
+
 	if err != nil {
 		t.Errorf("failed to get: %+v", err)
 	}
-	defer rsp.Body.Close()
-	got, err := io.ReadAll(rsp.Body)
+	defer response.Body.Close()
+	got, err := io.ReadAll(response.Body)
 	if err != nil {
 		t.Fatalf("failed to read body: %v", err)
 	}
