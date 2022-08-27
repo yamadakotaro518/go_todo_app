1ae682bdf3f019add2e3e863ab0c33cf39068e24
kotaro yamada <yamadakotaro@team-lab.com>
2022-08-27 19:40:08 +0900

    [fix]runメソッド変更に伴うmainメソッド修正

--- main/main.go
+++ main/main.go
@@ -7,34 +7,24 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	port := os.Args[1]
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", port, err)
+	}
+	if err := run(context.Background(), listener); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
-
-//func run(ctx context.Context) error {
-//	s := &http.Server{
-//		Addr: ":18080",
-//		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-//		}),
-//	}
-//	eg, ctx := errgroup.WithContext(ctx)
-//	eg.Go(func() error {
-//		if err := s.ListenAndServe(); err != nil &&
-//			err != http.ErrServerClosed {
-//			log.Printf("failed to close: %+v", err)
-//			return err
-//		}
-//		return nil
-//	})
-//
-//	<-ctx.Done()
-//	if err := s.Shutdown(context.Background()); err != nil {
-//		log.Printf("failed to shutdown: %+v", err)
-//	}
-//	return eg.Wait()
-//}
 
 func run(ctx context.Context, listener net.Listener) error {
 	s := &http.Server{
